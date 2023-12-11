630d4a85abfecd7314bd342ccdc2166841414ca1
JaviKotlinDev <javarbb.pn@gmail.com>
2023-12-11 23:05:19 +0100

    Add utils and gitignore.

---
+++ app/src/main/java/com/example/pokeapicompose/utils/NumberUtils.kt
@@ -0,0 +1,9 @@
+package com.example.pokeapicompose.utils
+
+fun Float?.orZero() = this ?: 0f
+
+fun Double?.orZero(): Double = this ?: 0.0
+
+fun Int?.orZero() = this ?: 0
+
+fun Long?.orZero() = this ?: 0L
