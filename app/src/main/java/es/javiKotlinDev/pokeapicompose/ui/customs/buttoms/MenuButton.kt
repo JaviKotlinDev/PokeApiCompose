179f9b70bad8e8a351de83788c1bfcba86a174b2
JaviKotlinDev <javarbb.pn@gmail.com>
2024-01-14 22:58:28 +0100

    Finish MenuOptions view. Continue with logic.

--- app/src/main/java/es/javiKotlinDev/pokeapicompose/ui/customs/buttoms/MenuButton.kt
+++ app/src/main/java/es/javiKotlinDev/pokeapicompose/ui/customs/buttoms/MenuButton.kt
@@ -1,42 +1,56 @@
 package es.javiKotlinDev.pokeapicompose.ui.customs.buttoms
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import es.javiKotlinDev.pokeapicompose.ui.theme.teal
+import es.javiKotlinDev.pokeapicompose.ui.theme.dark
+import es.javiKotlinDev.pokeapicompose.ui.theme.darkGray
 
 @Composable
 fun MenuButton(
-    onClick: () -> Unit
-) {
-
-}
-
-@Composable
-private fun CustomMenuButton(
-    modifier: Modifier = Modifier,
+    modifier: Modifier,
     text: String,
+    imageResId: Int,
     onClick: () -> Unit,
 ) {
     Button(
-        onClick = { onClick() },
+        onClick = onClick,
         colors = ButtonDefaults.buttonColors(
-            containerColor = teal,
+            containerColor = darkGray,
         ),
+        border = BorderStroke(1.dp, dark),
+        shape = RoundedCornerShape(5.dp),
         modifier = modifier
-            .wrapContentSize()
-            .padding(20.dp)
+            .aspectRatio(1f) // Se ajusta al espacio posible
     ) {
-        onClick()
-        Text(
-            text = text,
-            color = Color.Black
-        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(imageResId),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(100.dp)
+            )
+            Text(
+                text = text,
+                modifier = Modifier
+                    .padding(top = 8.dp)
+                    .wrapContentWidth(Alignment.CenterHorizontally),
+            )
+        }
     }
 }
--- app/src/main/java/es/javiKotlinDev/pokeapicompose/ui/menuOptions/MenuOptionsScreen.kt
+++ app/src/main/java/es/javiKotlinDev/pokeapicompose/ui/menuOptions/MenuOptionsScreen.kt
@@ -4,21 +4,22 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Transparent
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import es.javiKotlinDev.pokeapicompose.R
-import es.javiKotlinDev.pokeapicompose.ui.theme.teal
-import es.javiKotlinDev.pokeapicompose.ui.theme.white
+import es.javiKotlinDev.pokeapicompose.ui.customs.buttoms.MenuButton
+import es.javiKotlinDev.pokeapicompose.ui.theme.whiteTransparent
 
 @Composable
 fun MenuOptionsScreen(
@@ -26,18 +27,17 @@ fun MenuOptionsScreen(
     navigateToSearchPokemon: () -> Unit,
 ) {
     MenuBackground()
+    MenuOptions()
 }
 
-@Preview
 @Composable
 private fun MenuBackground() {
     Image(
         painter = painterResource(id = R.drawable.bg_login),
-        contentDescription = "",
+        contentDescription = null,
         contentScale = ContentScale.Crop,
         modifier = Modifier.fillMaxSize()
     )
-    MenuOptions()
 }
 
 @Composable
@@ -47,60 +47,88 @@ private fun MenuOptions(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(50.dp)
-            .background(white),
+            .padding(
+                top = 120.dp,
+                bottom = 120.dp,
+                start = 35.dp,
+                end = 35.dp
+            )
+            .background(whiteTransparent),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Row(
-            modifier = modifier
-                .padding(20.dp)
-                .background(teal)
-        ) {
-            // Primer botón en la primera fila
-            Button(
-                onClick = { /* Acción del botón 1 */ },
-                modifier = Modifier
-                    .padding(8.dp)
-                    .weight(1f) // Ocupa el 50% del ancho disponible
-            ) {
-                Text("Botón 1")
-            }
 
-            // Segundo botón en la primera fila
-            Button(
-                onClick = { /* Acción del botón 2 */ },
-                modifier = Modifier
+        Row(
+            modifier = Modifier
+                .background(Transparent)
+        ) {
+            MenuButton(
+                modifier = modifier
                     .padding(8.dp)
-                    .weight(1f) // Ocupa el 50% del ancho disponible
-            ) {
-                Text("Botón 2")
-            }
+                    .weight(1f),
+                text = "Pokemon",
+                imageResId = R.drawable.pokemon,
+                onClick = { /* Acción del botón 1 */ },
+            )
+
+            MenuButton(
+                modifier = modifier
+                    .padding(8.dp)
+                    .weight(1f),
+                text = "Pokedex",
+                imageResId = R.drawable.pokedex,
+                onClick = { /* Acción del botón 2 */ },
+            )
         }
 
-        Row(
-            modifier = modifier
-                .padding(20.dp)
-                .background(teal)
-        ) {
-            // Primer botón en la segunda fila
-            Button(
-                onClick = { /* Acción del botón 3 */ },
-                modifier = Modifier
-                    .padding(8.dp)
-                    .weight(1f) // Ocupa el 50% del ancho disponible
-            ) {
-                Text("Botón 3")
-            }
+        Spacer(modifier = Modifier.height(25.dp))
 
-            // Segundo botón en la segunda fila
-            Button(
-                onClick = { /* Acción del botón 4 */ },
-                modifier = Modifier
+        Row {
+            MenuButton(
+                modifier = modifier
                     .padding(8.dp)
-                    .weight(1f) // Ocupa el 50% del ancho disponible
-            ) {
-                Text("Botón 4")
-            }
+                    .weight(1f),
+                text = "Types",
+                imageResId = R.drawable.electric_logo,
+                onClick = { /* Acción del botón 3 */ },
+            )
+
+            MenuButton(
+                modifier = modifier
+                    .padding(8.dp)
+                    .weight(1f),
+                text = "Berries",
+                imageResId = R.drawable.berries,
+                onClick = { /* Acción del botón 4 */ },
+            )
+        }
+
+        Spacer(modifier = Modifier.height(25.dp))
+
+        Row {
+            MenuButton(
+                modifier = modifier
+                    .padding(8.dp)
+                    .weight(1f),
+                text = "Items",
+                imageResId = R.drawable.items,
+                onClick = { /* Acción del botón 5 */ },
+            )
+
+            MenuButton(
+                modifier = modifier
+                    .padding(8.dp)
+                    .weight(1f),
+                text = "Generations",
+                imageResId = R.drawable.generations,
+                onClick = { /* Acción del botón 6 */ },
+            )
         }
     }
+}
+
+@Preview
+@Composable
+private fun PreviewMenuOptions() {
+    MenuBackground()
+    MenuOptions()
 }
