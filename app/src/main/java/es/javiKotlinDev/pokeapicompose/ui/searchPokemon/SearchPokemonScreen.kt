aa72e304836b6ba4eae014a17f5f8d0671624a4a
JaviKotlinDev <javarbb.pn@gmail.com>
2024-01-22 18:28:19 +0100

    Finish search pokemon view

--- app/src/main/java/es/javiKotlinDev/pokeapicompose/ui/searchPokemon/SearchPokemonScreen.kt
+++ app/src/main/java/es/javiKotlinDev/pokeapicompose/ui/searchPokemon/SearchPokemonScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
